75cfc021f82be01147d71b488cef6c462aebad11
Tofaratty <86321568+Tofaratty@users.noreply.github.com>
2024-03-20 22:32:15 -0400

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -1,59 +1,42 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+/**
+ * POST /api/submit
+ */
 
-// This is the provided secret key. In production, it's recommended
-// to store secret keys securely.
+import turnstilePlugin from "@cloudflare/pages-plugin-turnstile";
+console.log(turnstilePlugin);
+
+
+// This is a demo secret key. In prod, we recommend you store
+// your secret key(s) safely. 
 const SECRET_KEY = '0x4AAAAAAAVKDcNri64lv50_A4eF-Z6WFKY';
 
-/**
- * Respond to the request
- * @param {Request} request
- */
-async function handleRequest(request) {
-  // Handle POST requests
-  if (request.method === 'POST') {
-    return handlePost(request)
-  }
+export const onRequestPost = [
+    turnstilePlugin({
+    	secret: SECRET_KEY,
+    }),
+    (async (context) => {
+    	// Request has been validated as coming from a human
+    	const formData = await context.request.formData()
 
-  // Return 405 Method Not Allowed for other request methods
-  return new Response('Method Not Allowed', { status: 405 })
-}
+    	var tmp, outcome = {};
+	for (let [key, value] of formData) {
+		tmp = outcome[key];
+		if (tmp === undefined) {
+			outcome[key] = value;
+		} else {
+			outcome[key] = [].concat(tmp, value);
+		}
+	}
 
-/**
- * Handle POST requests
- * @param {Request} request
- */
-async function handlePost(request) {
-  try {
-    const body = await request.formData();
-    // Turnstile injects a token in "cf-turnstile-response".
-    const token = body.get('cf-turnstile-response');
-    const ip = request.headers.get('CF-Connecting-IP');
+	// Attach Turnstile outcome to the response
+	outcome["turnstile_outcome"] = context.data.turnstile;
 
-    // Validate the token by calling the "/siteverify" API endpoint.
-    const formData = new FormData();
-    formData.append('secret', SECRET_KEY);
-    formData.append('response', token);
-    formData.append('remoteip', ip);
+	let pretty = JSON.stringify(outcome, null, 2);
 
-    const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
-    const result = await fetch(url, {
-      body: formData,
-      method: 'POST',
-    });
-
-    const outcome = await result.json();
-    if (outcome.success) {
-      // Token is valid, proceed with handling the request
-      // Implement your logic here
-      return new Response('Success', { status: 200 });
-    } else {
-      // Token is invalid
-      return new Response('Token validation failed', { status: 403 });
-    }
-  } catch (error) {
-    console.error('Error handling POST request:', error);
-    return new Response('Internal Server Error', { status: 500 });
-  }
-}
+      	return new Response(pretty, {
+      		headers: {
+      			'Content-Type': 'application/json;charset=utf-8'
+      		}
+      	});
+    })
+];
